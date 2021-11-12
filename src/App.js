e2c08643f72c2b73736c699f78b2587f95fcdb92
Ashish Sah <ashishsah173@gmail.com>
2021-11-12 12:15:05 +0530

    Backend added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./componenets/Video";
+import axios from "./axios/axios";
 
 function App() {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        const getVideos = async () => {
+            const response = await axios.get("/v1/posts");
+            setVideos(response.data);
+            return response;
+        };
+        getVideos();
+    }, []);
     return (
         <div className="app">
             <div className="app_videos">
-                <Video
-                    url="https://zippy.gfycat.com/BruisedPerkyFlyingsquirrel.mp4"
-                    channel="ashish"
-                    description="This is just a description"
-                    song="Come & get it"
-                    likes={100}
-                    messages={200}
-                    shares={50}
-                />
-                <Video
-                    url="https://zippy.gfycat.com/BruisedPerkyFlyingsquirrel.mp4"
-                    channel="ashish"
-                    description="This is just a description"
-                    song="Come & get it"
-                    likes={100}
-                    messages={200}
-                    shares={50}
-                />
-                <Video
-                    url="https://zippy.gfycat.com/BruisedPerkyFlyingsquirrel.mp4"
-                    channel="ashish"
-                    description="This is just a description"
-                    song="Come & get it"
-                    likes={100}
-                    messages={200}
-                    shares={50}
-                />
+                {videos.map((video) => (
+                    <Video
+                        url={video.url}
+                        channel={video.channel}
+                        description={video.description}
+                        song={video.song}
+                        likes={video.likes}
+                        messages={video.messages}
+                        shares={video.shares}
+                    />
+                ))}
             </div>
         </div>
     );
